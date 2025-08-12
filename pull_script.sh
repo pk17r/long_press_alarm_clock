667981f74a03309a68fa55ca382687006d4589e8
Prashant Kumar <prash.k20@gmail.com>
2025-08-11 23:15:53 -0700

    Release v4.0 binaries

---
+++ pull_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello, World!"
+cp ../long_press_alarm_clock_dev/build/esp32.esp32.lolin_s2_mini/long_press_alarm_clock_dev.ino.bin build/esp32.esp32.lolin_s2_mini/long_press_alarm_clock.ino.bin
+cp ../long_press_alarm_clock_dev/build/esp32.esp32.esp32s3/long_press_alarm_clock_dev.ino.bin build/esp32.esp32.esp32s3/long_press_alarm_clock.ino.bin
+cp ../long_press_alarm_clock_dev/build/esp32.esp32.esp32s2/long_press_alarm_clock_dev.ino.bin build/esp32.esp32.esp32s2/long_press_alarm_clock.ino.bin
+cp ../long_press_alarm_clock_dev/build/esp32.esp32.esp32da/long_press_alarm_clock_dev.ino.bin build/esp32.esp32.esp32da/long_press_alarm_clock.ino.bin
+echo "Copy Done!"
+
