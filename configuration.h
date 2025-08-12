4ad389b8614b83f7b5bd84046771fd8029c38d09
Prashant Kumar <prash.k20@gmail.com>
2025-08-11 23:41:56 -0700

    Auto time update if timeZoneId is blank after first update

--- configuration.h
+++ configuration.h
@@ -30,7 +30,7 @@
 #define ESP32_S2_FIRMWARE_VERSION                 "v4.0"
 #define ESP32_S3_FIRMWARE_VERSION                 "v4.0"
 #define ESP32_S2_MINI_FIRMWARE_VERSION            "v4.0"
-#define ESP32_WROOM_DA_MODULE_FIRMWARE_VERSION    "v3.9"
+#define ESP32_WROOM_DA_MODULE_FIRMWARE_VERSION    "v4.0"
 const std::string kFirmwareDate = "Aug 11, 2025";
 
 const std::string kChangeLog = "- Auto-Fetch of TimeZone!\n- DST Bug Correction\n- Other Improvements";
