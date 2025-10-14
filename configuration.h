62430c9f8327771e1af008f29b6e86ef7d5999ea
Prashant Kumar <prash.k20@gmail.com>
2025-10-14 02:20:40 -0700

    Release v4.3.1 binaries

--- configuration.h
+++ configuration.h
@@ -3,40 +3,21 @@
 
 #include <string>
 
-// SELECT MCU
-
-#define MCU_IS_ESP32_S2
+// #define MCU_IS_ESP32_S2
 // #define MCU_IS_ESP32_S3
-// #define MCU_IS_ESP32_S2_MINI
+#define MCU_IS_ESP32_S2_MINI
 // #define MCU_IS_ESP32_WROOM_DA_MODULE
-
-
-// SELECT DISPLAY
-
-#define DISPLAY_IS_ST7789V
-// #define DISPLAY_IS_ST7796
-// #define DISPLAY_IS_ST7735
-// #define DISPLAY_IS_ILI9341
-// #define DISPLAY_IS_ILI9488
-
-
-// SELECT IF WIFI IS USED
 
 #define WIFI_IS_USED
 
-
-// FIRMWARE VERSION   (update these when pushing new MCU specific binaries to github)
-
-#define ESP32_S2_FIRMWARE_VERSION                 "v4.3.0"
-#define ESP32_S3_FIRMWARE_VERSION                 "v4.3.0"
-#define ESP32_S2_MINI_FIRMWARE_VERSION            "v4.3.0"
+#define ESP32_S2_FIRMWARE_VERSION                 "v4.3.1"
+#define ESP32_S3_FIRMWARE_VERSION                 "v4.3.1"
+#define ESP32_S2_MINI_FIRMWARE_VERSION            "v4.3.1"
 #define ESP32_WROOM_DA_MODULE_FIRMWARE_VERSION    "v4.2.1"
-const std::string kFirmwareDate = "Oct 12, 2025";
+const std::string kFirmwareDate = "Oct 14, 2025";
 
-const std::string kChangeLog = "- Busy Symbol, No-WiFi Symbol!\n- Keyboard Enhancements!\n- UI & Software Enhancements";
-
+const std::string kChangeLog = "- Busy Symbol, No-WiFi Symbol!\n- Weather & TimeZone Btn\n- UI & Software Enhancements";
 
 // #define MORE_LOGS
-
 
 #endif  // CONFIGURATION_H
