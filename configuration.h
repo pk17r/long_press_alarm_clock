118c3959cc1206a046f3b7c688a4a4687f7bb7da
Prashant Kumar <prash.k20@gmail.com>
2025-10-14 23:59:58 -0700

    Release v4.3.0 binaries

--- configuration.h
+++ configuration.h
@@ -10,11 +10,11 @@
 
 #define WIFI_IS_USED
 
-#define ESP32_S2_FIRMWARE_VERSION                 "v4.3.1"
-#define ESP32_S3_FIRMWARE_VERSION                 "v4.3.1"
-#define ESP32_S2_MINI_FIRMWARE_VERSION            "v4.3.1"
+#define ESP32_S2_FIRMWARE_VERSION                 "v4.3.0"
+#define ESP32_S3_FIRMWARE_VERSION                 "v4.3.0"
+#define ESP32_S2_MINI_FIRMWARE_VERSION            "v4.3.0"
 #define ESP32_WROOM_DA_MODULE_FIRMWARE_VERSION    "v4.2.1"
-const std::string kFirmwareDate = "Oct 14, 2025";
+const std::string kFirmwareDate = "Oct 15, 2025";
 
 const std::string kChangeLog = "- Busy Symbol, No-WiFi Symbol!\n- Weather & TimeZone Btn\n- UI & Software Enhancements";
 
